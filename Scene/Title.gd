998f32fa6132fea7ae6effeaa6e4373725c339db
Nelio Jorge Quibe <79609481+NelioQuibe-258@users.noreply.github.com>
2023-06-21 12:37:13 -0700

    Update Title.gd

--- Scene/Title.gd
+++ Scene/Title.gd
@@ -1,29 +1,31 @@
 extends Node2D
 
-var sprite1: Sprite
-var sprite2: Sprite
 var startLabel: Label
+var spaceLabel: Label
+var joypads = Input.get_connected_joypads()
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	sprite1 = get_node("Sprite1")
-	sprite2 = get_node("Sprite2")
+	
+	#The two labels are not visible by default
 	startLabel = get_node("startLabel")
+	spaceLabel = get_node("spaceLabel")
 	
-	#getting the current connect controller
-	var joypads = Input.get_connected_joypads()
-	
+	#Setting the menu according to the connected device
 	if joypads.size() > 0:
-		
-		#Enabling "press start to start" when joystick is connected
-		sprite2.visible = true
 		startLabel.visible = true
-		
-		#desabling "press space to start" 
-		sprite1.visible = false
 	else:
-		sprite1.visible = true
-		sprite2.visible = false
+		spaceLabel.visible = true
+		
+	#Changing the menu dynamically when the joystick is connected or desconnected
+	for joypad in Input.get_connected_joypads():
+		print("Detected joypad device: ", joypad)
+	Input.connect("joy_connection_changed", self, "_on_joy_connection_changed")
+
+func _on_joy_connection_changed(device: int, connected: bool) -> void:
+	if connected:
+		startLabel.visible = true
+		spaceLabel.visible = false
+	else:
+		spaceLabel.visible = true
 		startLabel.visible = false
-
-
+		
