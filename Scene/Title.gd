9172866b85941f9de6a6e204cfcc0d913a1f9272
Nelio Jorge Quibe <79609481+NelioQuibe-258@users.noreply.github.com>
2023-06-26 01:54:50 +0000

    updated Title.dg as well as Title.tscn
    By default, there's no any text on the general label.
    
    When the game loads, it verifies which control is connected, afterwards it writes the text according to the device connected:
    "space" for keyboards
    "start" for joystick

--- Scene/Title.gd
+++ Scene/Title.gd
@@ -4,11 +4,21 @@ var generalLabel: Label
 
 func _ready():
 	generalLabel = get_node("generalLabel")
+	
+	identifyInputOnLoad()
 
 	Input.connect("joy_connection_changed", self, "_on_joy_connection_changed")
 
+#Real time Input controller device identification
 func _on_joy_connection_changed(device_id: int, connected: bool):
 	if connected:
+		generalLabel.text = "START";
+	else:
+		generalLabel.text = "SPACE";
+		
+#When the game loads, it automatically identify the Input device controller
+func identifyInputOnLoad():
+	if Input.is_joy_known(0):
 		generalLabel.text = "START";
 	else:
 		generalLabel.text = "SPACE";
