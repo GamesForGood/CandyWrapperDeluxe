741379bbc38920956c2d347db2bf09ed3aa5e2c6
Filipe Castro <filipe_bilipe@hotmail.com>
2023-08-25 17:14:44 -0600

    Change how audio OST is controlled, put it in Global to simplify.

--- Script/Global.gd
+++ Script/Global.gd
@@ -10,3 +10,15 @@ var lastLevel = 21
 
 var Main
 var Game
+
+var OST = load("res://Audio/OST.ogg")
+var audio
+
+func _ready():
+	await get_tree().create_timer(0.1).timeout
+	
+	audio = AudioStreamPlayer.new()
+	add_child(audio)
+	audio.stream = OST
+	audio.play()
+	audio.finished.connect(audio.play)
